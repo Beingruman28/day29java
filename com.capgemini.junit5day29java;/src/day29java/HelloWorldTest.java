789ad18ad60dbf557b663a3e28b8d25ba873fa86
ruman <mohammedruman1996@gmail.com>
2022-03-08 15:04:10 +0530

    day29

---
+++ com.capgemini.junit5day29java;/src/day29java/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package day29java;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest implements TestLifecycleLogger
+{
+
+	@Test
+	@RepeatedTest(10)
+	void test() 
+	{
+		System.out.println("First Test Case");
+	}
+
+	
+
+}
---
+++ com.capgemini.junit5day29java;/src/day29java/TestLifecycleLogger.java
@@ -0,0 +1,5 @@
+package day29java;
+
+public interface TestLifecycleLogger {
+
+}
